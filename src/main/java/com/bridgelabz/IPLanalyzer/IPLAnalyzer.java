2ca4218e48947f97ce5e6a9a9f15497987ffab5e
TanmayJain97 <mail.tanmay.jain@gmail.com>
2020-10-27 19:32:58 +0530

    Add: Function for reading Bowler Data

--- src/main/java/com/bridgelabz/IPLanalyzer/IPLAnalyzer.java
+++ src/main/java/com/bridgelabz/IPLanalyzer/IPLAnalyzer.java
@@ -6,25 +6,28 @@ import java.nio.file.Paths;
 import java.util.*;
 
 import com.bridgelabz.IPLanalyzer.model.BatsmanData;
+import com.bridgelabz.IPLanalyzer.model.BowlerData;
 import com.bridgelabz.IPLanalyzer.sortingFunction.BatsmanSortingFunction;
+import com.bridgelabz.IPLanalyzer.sortingFunction.BowlerSortingFunction;
 import com.bridgelabz.censusAnalyzer.csvbuilder.CSVBuilderFactory;
 import com.bridgelabz.censusAnalyzer.csvbuilder.ICSVBuilder;
 import com.bridgelabz.censusAnalyzer.csvbuilder.exception.CSVBuilderException;
 import com.bridgelabz.censusAnalyzer.csvbuilder.exception.CSVBuilderException.ExceptionType;
 
 public class IPLAnalyzer {
-	
+
 	List<BatsmanData> battingList;
-	
+	List<BowlerData> bowlingList;
+
 	public void readBatsmanData(String FilePath) throws CSVBuilderException{
-		
+
 		try {
 			Files.newBufferedReader(Paths.get(FilePath));
 			Reader reader=Files.newBufferedReader(Paths.get(FilePath));
-			
+
 			ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
 			battingList=csvBuilder.getCSVList(reader, BatsmanData.class);
-			
+
 		}catch(IOException exception) {
 			throw new CSVBuilderException("Invalid Path Name",
 					ExceptionType.INVALID_FILE_PATH);
@@ -33,16 +36,42 @@ public class IPLAnalyzer {
 					ExceptionType.INVALID_DELIM_OR_HEAD);
 		}
 	}
-	
+
+	public void readBowlerData(String FilePath) throws CSVBuilderException{
+
+		try {
+			Files.newBufferedReader(Paths.get(FilePath));
+			Reader reader=Files.newBufferedReader(Paths.get(FilePath));
+
+			ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+			bowlingList=csvBuilder.getCSVList(reader, BowlerData.class);
+
+		}catch(IOException exception) {
+			throw new CSVBuilderException("Invalid Path Name",
+					ExceptionType.INVALID_FILE_PATH);
+		}catch(RuntimeException exception){
+			throw new CSVBuilderException("Invalid Delimitor/Head Name",
+					ExceptionType.INVALID_DELIM_OR_HEAD);
+		}
+	}
+
 	public int getCount(List list) {
 		return list.size();
 	}
-	
-	public List<BatsmanData> getSortedList(BatsmanSortingFunction.Order sortOrder) throws CSVBuilderException{
+
+	public List<BatsmanData> getBatsmanSortedList(BatsmanSortingFunction.Order sortOrder) throws CSVBuilderException{
 		if(battingList==null||battingList.size()==0)
 			throw new CSVBuilderException("No Census Data", ExceptionType.NO_DATA);
 		BatsmanSortingFunction sort=new BatsmanSortingFunction(sortOrder);
 		Collections.sort(battingList,sort);
 		return battingList;
+	}
+	
+	public List<BowlerData> getBowlerSortedList(BowlerSortingFunction.Order sortOrder) throws CSVBuilderException{
+		if(bowlingList==null||bowlingList.size()==0)
+			throw new CSVBuilderException("No Census Data", ExceptionType.NO_DATA);
+		BowlerSortingFunction sort=new BowlerSortingFunction(sortOrder);
+		Collections.sort(bowlingList,sort);
+		return bowlingList;
 	}
 }
